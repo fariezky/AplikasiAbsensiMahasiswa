1c9aade2d0fe4dcc860325b66363c77549f89f6f
Bekti Prihanto <48426988+bektiprihanto@users.noreply.github.com>
2020-12-13 11:26:59 +0700

    Membuat fungsi insert, save, create new data

--- index.py
+++ index.py
@@ -57,3 +57,53 @@ D3 = sheet['D3']
 D3.font = font
 D3.border = border
 D3.alignment = alignment
+
+num = 0
+
+
+def InsertData():
+    global num
+    num = num + 1
+    sheetnum = num + 3
+
+    sheet['A'+str(sheetnum)] = num
+    DataNo = sheet['A'+str(sheetnum)]
+    DataNo.border = border
+    DataNo.alignment = alignment
+
+    sheet['B'+str(sheetnum)] = namaEntry.get()
+    DataNama = sheet['B'+str(sheetnum)]
+    DataNama.border = border
+    DataNama.alignment = alignment
+
+    sheet['C' + str(sheetnum)] = NIMEntry.get()
+    DataNIM = sheet['C' + str(sheetnum)]
+    DataNIM.border = border
+    DataNIM.alignment = alignment
+
+    sheet['D' + str(sheetnum)] = jurusanEntry.get()
+    DataJurusan = sheet['D' + str(sheetnum)]
+    DataJurusan.border = border
+    DataJurusan.alignment = alignment
+
+    sheet['B1'] = matkulEntry.get()
+    sheet['B2'] = tanggalEntry.get()
+
+    namaEntry.delete(0, END)
+    NIMEntry.delete(0, END)
+    jurusanEntry.delete(0, END)
+
+def SaveData():
+    global informasi
+    workbook.save(filename=str(matkulEntry.get())+"_"+str(tanggalEntry.get())+".xlsx")
+    informasi['text'] = "Data absen telah di save!\nNama file: "+str(matkulEntry.get())+"_"+str(tanggalEntry.get())+".xlsx"
+
+def CreateNewData():
+    global informasi, num
+    informasi['text'] = 'Klik Insert untuk semua mahasiswa, kemudian klik Save jika semua telah diabsen.'
+    namaEntry.delete(0, END)
+    NIMEntry.delete(0, END)
+    jurusanEntry.delete(0, END)
+    matkulEntry.delete(0, END)
+    tanggalEntry.delete(0, END)
+    num = 0
